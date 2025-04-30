f6c849a3c6176fe21c9c98d9d977946b4f8fbd99
Aashi <achaubey@uwaterloo.ca>
2025-04-30 15:32:29 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import styles from "./App.css";
 import Hero from "./Hero";
 import NavBar from "./NavBar";
 import About from "./About";
-
+import ParticlesComponent from './particles';
 import "@fontsource/poppins";
+
 function App(){
   return (
-    <div className = {styles.App}>
-      <NavBar />
-      <Hero />
-      <About />
+    <div className={styles.App}>
+      <ParticlesComponent id="particles" />
+      <div className={styles.content}>
+        <NavBar />
+        <Hero />
+        <About />
+      </div>
     </div>
   );
 }
